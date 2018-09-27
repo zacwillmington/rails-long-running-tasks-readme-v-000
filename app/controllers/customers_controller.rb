928a2e2f68f36963bdd5a29dd48fb58f13f9bde7
Zac Willmington <zac.willmington@gmail.com>
2018-09-27 22:01:04 +0000

    automatically backed up by learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,11 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to customers_path
+  end
+
 end
